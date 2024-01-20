e0da406beff92bc2ccf2d5ccebf47a4596a2ea50
Trijay1709 <trijaypatel1709@gmail.com>
2024-01-21 03:00:54 +0530

    event card gsap

--- src/components/event/cards/card.tsx
+++ src/components/event/cards/card.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
-import { Separator } from '@/components/ui/separator';
-import './card.css'; // Import the CSS file for styling
-import { Button } from '@/components/ui/button';
+import React, { useRef, useState } from "react";
+import gsap from "gsap";
+import { Separator } from "@/components/ui/separator";
+import "./card.css"; // Import the CSS file for styling
+import { Button } from "@/components/ui/button";
+import { ArrowDown, ArrowUp } from "lucide-react";
 
 interface EventCardProps {
   title: string;
@@ -12,30 +14,99 @@ interface EventCardProps {
   description: string;
 }
 
-const EventCard: React.FC<EventCardProps> = ({ title, img, startDate, endDate, registerUrl, description }) => {
+const EventCard: React.FC<EventCardProps> = ({
+  title,
+  img,
+  startDate,
+  endDate,
+  registerUrl,
+  description,
+}) => {
+  const cardRef = useRef(null);
+  const [expanded, setExpanded] = useState(true);
 
   const handleRegisterClick = () => {
     window.location.href = registerUrl;
   };
 
+  const handleMoreClick = () => {
+    const card = cardRef.current;
+
+    if (!expanded) {
+      // If card is not expanded, expand it
+      gsap.from(card, { height: "auto" });
+      gsap.to(card, { height: "22rem", duration: 0.5 });
+    } else {
+      gsap.from(card, { height: "22rem" });
+
+      // If card is expanded, collapse it
+      gsap.to(card, { height: "auto", duration: 0.5 });
+    }
+
+    setExpanded(!expanded);
+  };
+
   return (
-    <article className="event-card flex flex-row gap-5 mb-20 p-8 border-orange-500 border-2 rounded-lg w-[48%] transition-transform transform hover:scale-105 hover:shadow-lg hover:shadow-[#FFA500]">
-      <div className='image-container'>
-        <img src={img} alt={`Event: ${title}`} className='event-image w-[30rem] rounded-md ' />
+    <article
+      ref={cardRef}
+      className={`event-card flex flex-row gap-5 mb-20 p-8 border-orange-500 border-2 rounded-lg w-[90%] transition-transform transform hover:scale-105 hover:shadow-lg hover:shadow-[#FFA500] h-[22rem]`}
+    >
+      <div className="image-container">
+        <img
+          src={img}
+          alt={`Event: ${title}`}
+          className="event-image w-[12rem] rounded-md "
+        />
       </div>
-      <div className={`event-details-container w-[36rem]`}>
-        <header className='event-title text-3xl mb-3 font-bold'>{title}</header>
-        <div className='event-details'>
+      <div className={`event-details-container w-[80%]`}>
+        <header className="event-title text-3xl mb-3 font-bold">{title}</header>
+        <div className="event-details">
           <Separator />
-          <p className='event-dates text-2xl mb-3'>{startDate} - {endDate}</p>
-          <p className='event-description text-justify text-lg'>{description}</p>
-          <div className='event-buttons flex items-center justify-center'>
-            <Button className='register-button px-6 py-4 text-lg mt-4'
-              // variant="secondary"
-              onClick={handleRegisterClick}
-            >
-              Register
-            </Button>
+          <p className="event-dates text-2xl mb-3">
+            {startDate} - {endDate}
+          </p>
+          <p className="event-description text-justify text-lg">
+            {description}
+          </p>
+          {/* Additional content for demonstration */}
+          {!expanded && (
+            <div className="expaneded-content mt-4 text-lg">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+                vitae facilisis risus. Nullam nec dolor nec elit convallis
+                dignissim.
+              </p>
+              <p>
+                Vestibulum efficitur quam vitae justo consectetur ultrices.
+                Nullam sed metus quis odio bibendum dignissim.
+              </p>
+              <p>
+                Duis facilisis mauris sit amet nibh rhoncus, at lobortis velit
+                scelerisque. In hac habitasse platea dictumst.
+              </p>
+            </div>
+          )}
+          {/* End of additional content */}
+          <div className="event-buttons grid grid-cols-3 gap-4 items-center ">
+            <div className="col-span-1 col-end-3 text-xl">
+              <button
+                onClick={handleMoreClick}
+                className="flex items-center justify-center gap-x-2 hover:underline"
+              >
+                
+                
+                {!expanded ? <ArrowUp />  : <ArrowDown /> }
+                {!expanded ? "Less" : "More"}
+              </button>
+            </div>
+            <div className=" col-span-1 col-end-4">
+              <Button
+                className="register-button px-10 py-7 text-xl mt-4"
+                onClick={handleRegisterClick}
+              >
+                Register
+              </Button>
+            </div>
           </div>
         </div>
       </div>
