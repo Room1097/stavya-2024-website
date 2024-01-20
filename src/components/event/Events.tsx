3c347d6472b3442a060514a86524640429a6fdb5
Trijay1709 <trijaypatel1709@gmail.com>
2024-01-20 13:32:01 +0530

    Event Card hover

--- src/components/event/Events.tsx
+++ src/components/event/Events.tsx
@@ -1,7 +1,43 @@
 import EventCard from "./cards/card";
+import { Separator } from "../ui/separator";
 
 const Events = () => {
     const data = [
+        {
+            img:"https://www.freecodecamp.org/news/content/images/2021/08/imgTag.png",
+            registerUrl:"https://www.google.com",
+            title:"Stavya",
+            startDate:"26 Jan",
+            endDate:"30 Jan",
+        },
+        {
+            img:"https://www.freecodecamp.org/news/content/images/2021/08/imgTag.png",
+            registerUrl:"https://www.google.com",
+            title:"Stavya",
+            startDate:"26 Jan",
+            endDate:"30 Jan",
+        },
+        {
+            img:"https://www.freecodecamp.org/news/content/images/2021/08/imgTag.png",
+            registerUrl:"https://www.google.com",
+            title:"Stavya",
+            startDate:"26 Jan",
+            endDate:"30 Jan",
+        },
+        {
+            img:"https://www.freecodecamp.org/news/content/images/2021/08/imgTag.png",
+            registerUrl:"https://www.google.com",
+            title:"Stavya",
+            startDate:"26 Jan",
+            endDate:"30 Jan",
+        },
+        {
+            img:"https://www.freecodecamp.org/news/content/images/2021/08/imgTag.png",
+            registerUrl:"https://www.google.com",
+            title:"Stavya",
+            startDate:"26 Jan",
+            endDate:"30 Jan",
+        },
         {
             img:"https://www.freecodecamp.org/news/content/images/2021/08/imgTag.png",
             registerUrl:"https://www.google.com",
@@ -20,7 +56,15 @@ const Events = () => {
   return (
 
     <>
-      <div className="h-[35rem] border-red-500 border-2 mt-48 flex gap-x-32 flex-wrap p-8">
+      <div className="w-full flex justify-center items-center text-white font-mono h-24 text-4xl">
+        Events for Stavya 2K24
+
+      </div>
+      <Separator
+      className="w-[80vw] m-auto"
+      />
+      <div className="h-[35rem] mt-48 flex gap-x-16 gap-y-14 flex-wrap p-12 justify-around">
+
       {
             data.map((item,index)=>(
                 <EventCard img={item.img} registerUrl={item.registerUrl} startDate={item.startDate} endDate={item.endDate} title={item.title}/>
--- src/components/event/cards/card.tsx
+++ src/components/event/cards/card.tsx
@@ -31,17 +31,20 @@ const EventCard: React.FC<EventCardProps> = ({
 
   return (
     <>
-      <div className="h-[24rem] w-[17rem] bg-slate-300 rounded-lg flex flex-col items-center p-5">
-        <img src={img} className="h-32 w-48 rounded-md" />
+      <div className="h-[30rem] w-[20rem] bg-slate-300 rounded-lg flex flex-col items-center p-8 transition-all transform hover:scale-105 hover:shadow-lg">
+        <img
+          src={img}
+          className="h-48 rounded-md transition-all transform hover:scale-110"
+        />
         <div className="grid grid-rows-3 items-center justify-center p-3 h-full">
           <div className="flex justify-center">
             <div className="text-black text-2xl font-semibold">{title}</div>
           </div>
-          <div className="text-black text-lg flex mt-3 font-semibold">
+          <div className="text-black text-lg flex justify-center mt-3 font-semibold transition-all transform hover:scale-110">
             <Calendar className="h-6 w-6 mr-3" /> <div>{renderDate()}</div>
           </div>
-          <div className="mt-3 flex justify-center">
-            <Button variant="secondary" onClick={handleRegisterClick}>
+          <div className="mt-7 flex justify-center">
+            <Button variant="secondary" onClick={handleRegisterClick} className="w-[18rem] h-[4rem] text-lg">
               Register Here !!
             </Button>
           </div>
