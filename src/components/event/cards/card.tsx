ee0e56d06ebeb85bd51df4de2a5c176dce21edf9
YuvrajChauhan1303 <email.yuvraj.13@gmail.com>
2024-01-21 11:31:49 +0530

    adding responsiveness to event card page

--- src/components/event/cards/card.tsx
+++ src/components/event/cards/card.tsx
@@ -35,9 +35,9 @@ const EventCard: React.FC<EventCardProps> = ({
     if (!expanded) {
       // If card is not expanded, expand it
       gsap.from(card, { height: "auto" });
-      gsap.to(card, { height: "22rem", duration: 0.5 });
+      gsap.to(card, { height: "24rem", duration: 0.5 });
     } else {
-      gsap.from(card, { height: "22rem" });
+      gsap.from(card, { height: "24rem"});
 
       // If card is expanded, collapse it
       gsap.to(card, { height: "auto", duration: 0.5 });
@@ -49,23 +49,25 @@ const EventCard: React.FC<EventCardProps> = ({
   return (
     <article
       ref={cardRef}
-      className={`event-card flex flex-row gap-5 mb-20 p-8 border-orange-500 border-2 rounded-lg w-[90%] transition-transform transform hover:scale-105 hover:shadow-lg hover:shadow-[#FFA500] h-[22rem]`}
+      className={`event-card flex lg:flex-row gap-5 mb-20 lg:p-8 border-orange-500 border-2 rounded-lg lg:w-[90%] transition-transform transform hover:scale-105 hover:shadow-lg hover:shadow-[#FFA500] lg:h-[22rem] event__card`}
     >
       <div className="image-container">
         <img
           src={img}
           alt={`Event: ${title}`}
-          className="event-image w-[12rem] rounded-md "
+          className="event-image lg:w-[12rem] rounded-md "
         />
       </div>
-      <div className={`event-details-container w-[80%]`}>
-        <header className="event-title text-3xl mb-3 font-bold">{title}</header>
+      <div className={`event-details-container w-[80%] h-auto`}>
+        <header className="event-title text-3xl mb-3 font-bold">
+          <h1 className="event-title">{title}</h1>
+        </header>
         <div className="event-details">
           <Separator />
-          <p className="event-dates text-2xl mb-3">
+          <p className="event-dates lg:text-2xl mb-3">
             {startDate} - {endDate}
           </p>
-          <p className="event-description text-justify text-lg">
+          <p className="event-description text-justify lg:text-lg">
             {description}
           </p>
           {/* Additional content for demonstration */}
@@ -101,7 +103,7 @@ const EventCard: React.FC<EventCardProps> = ({
             </div>
             <div className=" col-span-1 col-end-4">
               <Button
-                className="register-button px-10 py-7 text-xl mt-4"
+                className="register-button lg:px-10 lg:py-7 lg:text-xl lg:mt-4"
                 onClick={handleRegisterClick}
               >
                 Register
@@ -110,6 +112,7 @@ const EventCard: React.FC<EventCardProps> = ({
           </div>
         </div>
       </div>
+
     </article>
   );
 };
