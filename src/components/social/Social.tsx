dfb43e1bb0f61db5eaeba9a10e342e4a821d55ac
YuvrajChauhan1303 <email.yuvraj.13@gmail.com>
2024-01-21 12:26:57 +0530

    added responsiveness to footer

--- src/components/social/Social.tsx
+++ src/components/social/Social.tsx
@@ -10,16 +10,16 @@ interface SocialProps {
 
 const Social: React.FC<SocialProps> = ({ committeeName, gmailLink, instagramLink }) => {
   return (
-    <div className='flex flex-col justify-center items-center'>
+    <div className='flex flex-col justify-center items-center social-container'>
       <div>
-        <h1 className='text-3xl mb-3'>{committeeName} Committee</h1>
+        <h1 className='lg:text-3xl mb-3'>{committeeName} Committee</h1>
         <Separator className='bg-zinc-700 mb-4'/>
-        <div className='links mt-2 flex flex-col gap-y-4'>
+        <div className='links mt-2 flex flex-col lg:gap-y-4 gap-y-2'>
           <div className='flex gap-4'>
-            <Mail /> <a href={gmailLink} className='text-xl duration-300' target='_blank' rel='noopener noreferrer'>Email Us</a>
+            <Mail /> <a href={gmailLink} className='lg:text-xl duration-300 text-sm' target='_blank' rel='noopener noreferrer'>Email Us</a>
           </div>
           <div className='flex gap-4'>
-            <Instagram /> <a href={instagramLink} className='text-xl duration-300' target='_blank' rel='noopener noreferrer'>Instagram</a>
+            <Instagram /> <a href={instagramLink} className='lg:text-xl text-sm duration-300' target='_blank' rel='noopener noreferrer'>Instagram</a>
           </div>
         </div>
       </div>
