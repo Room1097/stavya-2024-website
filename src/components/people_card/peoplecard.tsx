609ce3d1fd31c3b0a6184c85d5f8b13337f8ebff
YuvrajChauhan1303 <email.yuvraj.13@gmail.com>
2024-01-21 14:51:02 +0530

    added animations to people card

--- src/components/people_card/peoplecard.tsx
+++ src/components/people_card/peoplecard.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import './person.css';
 import { Separator } from '../ui/separator';
 
@@ -9,20 +10,19 @@ interface PeopleCardProps {
 }
 
 const PeopleCard: React.FC<PeopleCardProps> = ({ image, name, team, contact }) => {
- 
-
-
   return (
-    <div className='person-card slide__bottom' >
-      <div className="profile-pic delay1 show" >
+    <div className="person-card-container">
+      <div className='person-card slide__bottom person-card-slide'>
+      <div className="profile-pic delay1 show">
         <img src={image} alt="Profile" className='profile' />
       </div>
-      <div className="details show2" >
+      <div className="details show2">
         <h2 className='details__name'>{name}</h2>
         <Separator />
         <h3>{team}</h3>
         <h3>{contact}</h3>
       </div>
+    </div>
     </div>
   );
 };
