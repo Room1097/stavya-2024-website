56f1434c71a562de09660b26ce616ccae8735fc6
Trijay1709 <trijaypatel1709@gmail.com>
2024-01-24 02:39:27 +0530

    Update mousefollower.tsx

--- src/components/mousemover/mousefollower.tsx
+++ src/components/mousemover/mousefollower.tsx
@@ -1,37 +1,4 @@
-// // MouseFollower.tsx
 
-// import React, { useState, useEffect } from 'react';
-
-// import './mousefollwer.css'; // Import Tailwind CSS
-
-// const MouseFollower: React.FC = () => {
-//   const [position, setPosition] = useState({ x: 0, y: 0 });
-
-//   useEffect(() => {
-//     const handleMouseMove = (event: MouseEvent) => {
-//       // Update the position of the circle with a slight delay
-//       setTimeout(() => {
-//         setPosition({ x: event.clientX, y: event.clientY + window.scrollY });
-//       }, 100);
-//     };
-
-//     window.addEventListener('mousemove', handleMouseMove);
-
-//     // Cleanup event listener on component unmount
-//     return () => {
-//       window.removeEventListener('mousemove', handleMouseMove);
-//     };
-//   }, []);
-
-//   return (
-//     <div className="mouse-follower" style={{ left: position.x, top: position.y }}>
-//       {/* Your circle content */}
-//     </div>
-//   );
-// };
-
-// export default MouseFollower;
-// CircleCursor.tsx
 import React, { useState, useEffect } from 'react';
 import './mousefollwer.css';
 
