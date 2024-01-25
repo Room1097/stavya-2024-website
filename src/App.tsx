d17047249eca2353e2ceb6590ebb8411ff42fee2
YuvrajChauhan1303 <email.yuvraj.13@gmail.com>
2024-01-25 19:58:54 +0530

    changed browser router to hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { Routes, Route, HashRouter} from 'react-router-dom'
 import Home  from './pages/home';
 import About  from './pages/about';
 import Eventpage from './pages/events';
@@ -9,15 +9,15 @@ import MouseFollower from './components/mousemover/mousefollower';
 function App() {
     return (
         <>
-          <BrowserRouter>
+          <HashRouter>
             <Routes>
               <Route index element = {<Home /> } />
-              <Route path='/' element =  {<Home /> } />
+              <Route path='/*' element =  {<Home /> } />
               <Route path='/about' element =  {<About /> } />
               <Route path='/events' element =  {<Eventpage /> } />
               <Route path='/gallery' element =  {<GalleryPage /> } />
             </Routes>
-          </BrowserRouter>  
+          </HashRouter>  
           <MouseFollower /> 
         </>
     );
