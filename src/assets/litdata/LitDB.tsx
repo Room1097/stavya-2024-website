f6b91fa75de0e3754859672d454d5c7e78c855c4
YuvrajChauhan1303 <email.yuvraj.13@gmail.com>
2024-01-25 12:29:28 +0530

    updated paths

--- src/assets/litdata/LitDB.tsx
+++ src/assets/litdata/LitDB.tsx
@@ -2,35 +2,35 @@
 export const LitDB = [
     {
       "name": "Srijan Sharma",
-      "image": "public/img/litpics/Srijan.jpg",
+      "image": "img/litpics/Srijan.jpg",
       "team" : "Literature Committee",
       "insta": "202211084@iiitvadodara.ac.in",
       "linkedin": "https://www.linkedin.com/in/srijan-sharma-10389626a?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
     },
     {
       "name": "Anay Pandey",
-      "image": "public/img/litpics/AnayKumarPandey.png",
+      "image": "img/litpics/AnayKumarPandey.png",
       "team" : "Literature Committee",
       "insta": "202211063@iiitvadodara.ac.in",
       "linkedin": "https://www.linkedin.com/in/anaykumar-pandey-049336259/"
     },
     {
       "name": "Tanay Patel",
-      "image": "public/img/litpics/Tanay.jpg",
+      "image": "img/litpics/Tanay.jpg",
       "team" : "Literature Committee",
       "linkedin": "https://www.linkedin.com/in/tanay-patel-335418259?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
       "insta": "202211094@iiitvadodara.ac.in"
     },
     {
       "name": "Devrikh Jatav",
-      "image": "public/img/litpics/DevrikhJatav.jpg",
+      "image": "img/litpics/DevrikhJatav.jpg",
       "team" : "Literature Committee",
       "linkedin": "https://www.linkedin.com/in/devrikh-jatav-6bb512256",
       "insta": "202211018@iiitvadodara.ac.in"
     },
     {
       "name": "Ved Prakash Meena",
-      "image": "public/img/litpics/Ved.jpg",
+      "image": "img/litpics/Ved.jpg",
       "team" : "Literature Committee",
       "linkedin": "https://www.linkedin.com/in/ved-prakash-meena?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
       "insta": "202211096@iiitvadodara.ac.in"
