f433220c1be1ae213022735d232f4c7b18f3ca12
YuvrajChauhan1303 <email.yuvraj.13@gmail.com>
2024-01-25 19:08:18 +0530

    made changes to footer

--- src/components/social/Social.tsx
+++ src/components/social/Social.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Separator } from '@/components/ui/separator';
-import { Mail, Instagram } from 'lucide-react';
+import { Mail, Instagram, Ghost } from 'lucide-react';
 import { Button } from '../ui/button';
 
 interface SocialProps {
@@ -17,10 +17,14 @@ const Social: React.FC<SocialProps> = ({ committeeName, gmailLink, instagramLink
         <Separator className='bg-zinc-700 mb-4'/>
         <div className='links mt-2 flex flex-col lg:gap-y-4 gap-y-2'>
           <div className='flex gap-4'>
-            <Mail /> <Button onClick={()=>{window.location.href=`mailto:${gmailLink}`}} className='lg:text-xl duration-300 text-sm'  rel='noopener noreferrer'>Email Us</Button>
+            <Mail /> <Button 
+            variant="link"
+            onClick={()=>{window.location.href=`mailto:${gmailLink}`}} 
+            className='lg:text-xl duration-300 text-sm pb-[1.5rem] ml-[-1rem] font-normal'  
+            rel='noopener noreferrer'>Email Us</Button>
           </div>
           <div className='flex gap-4'>
-            <Instagram /> <a href={instagramLink} className='lg:text-xl text-sm duration-300' target='_blank' rel='noopener noreferrer'>Instagram</a>
+            <Instagram /> <a href={instagramLink} className='lg:text-xl text-sm duration-300 hover:underline' target='_blank' rel='noopener noreferrer'>Instagram</a>
           </div>
         </div>
       </div>
